a34fd1f54daaea30f0042fe5d84684de1a0a0b1d
hhy-ccj <772958417@qq.com>
2020-08-04 08:17:09 +0800

    添加管理员运行工具

---
+++ vimfiles/tools/admin.bat
@@ -0,0 +1,7 @@
+@echo off
+
+@echo off&(cd/d "%~dp0")&(cacls "%SystemDrive%\System Volume Information" >nul 2>&1)||(start "" mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&exit /b)
+
+:Admin
+    cd/d %1
+    call %2
