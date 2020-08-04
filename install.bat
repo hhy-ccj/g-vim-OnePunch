38e2dd8b52078db134e112618eba3128aef33194
hhy-ccj <772958417@qq.com>
2020-08-05 00:47:23 +0800

    优化admin.bat调用参数

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
 @echo off
 
-:: 设置当前 cmd 的编码格式为 UTF-8
+:: 设置当前 cmd 的编码格式为 UTF-8, 即可以用 echo 输出中文
 :: >nul 表示屏蔽操作成功显示的信息, 但是出错还是会显示
 chcp 65001 >nul
 
-:: -- 管理员模式安装
+:: 管理员模式安装
 pushd .
-call .\vimfiles\tools\admin.bat %~dp0 WindowsInstall.bat
+call .\vimfiles\tools\admin.bat WindowsInstall.bat
 popd
--- vimfiles/tools/admin.bat
+++ vimfiles/tools/admin.bat
@@ -1,7 +1,25 @@
 @echo off
 
+setlocal enabledelayedexpansion
+set current_bat_path=%~dp0
+set SourceCmdPath_file=%current_bat_path%\SourceCmdPath
+set Target_bat=%1
+
+if exist "%SourceCmdPath_file%" (
+    for /f "delims=" %%a in (%SourceCmdPath_file%) do (
+        set SourceCmdPath=%%a
+        REM echo !SourceCmdPath!
+    )
+    del %SourceCmdPath_file%
+) else (
+    set SourceCmdPath=!cd!
+    REM echo !SourceCmdPath!
+    echo !SourceCmdPath! >> %SourceCmdPath_file%
+)
+
 @echo off&(cd/d "%~dp0")&(cacls "%SystemDrive%\System Volume Information" >nul 2>&1)||(start "" mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&exit /b)
 
 :Admin
-    cd/d %1
-    call %2
+    setlocal disabledelayedexpansion
+    cd/d %SourceCmdPath%
+    call %Target_bat%
